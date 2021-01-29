e94377638c63f37f1f2abd63403de4b20d69f75b
Bui Manh <buimanh17112001@gmail.com>
2021-01-29 18:08:10 +0700

    ud

--- index.js
+++ index.js
@@ -16,15 +16,21 @@ class Model {
     this.light;
     this.camera;
     this.engine;
-    this.mainLight;
 
-    this.box = null;
+    this.cameraTarget = {
+      x: 0,
+      y: 0,
+      z: 0
+    }
 
     this.init();
   }
 
   init() {
     //create main components
+
+    LOADING.classList.add('active');
+
     this.canvas = document.createElement('canvas');
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
@@ -34,8 +40,9 @@ class Model {
 
     this.scene = new BABYLON.Scene(this.engine);
 
-    this.camera = new BABYLON.ArcRotateCamera("Camera", BABYLON.Tools.ToRadians(120), 3 * Math.PI / 8, 1000, new BABYLON.Vector3(0, 0, 0), this.scene);
+    this.camera = new BABYLON.ArcRotateCamera("Camera", BABYLON.Tools.ToRadians(120), 3 * Math.PI / 8, 800, new BABYLON.Vector3(0, 0, 0), this.scene);
     console.log(this.camera);
+
 
     this.createScene();
 
@@ -50,7 +57,7 @@ class Model {
         let z = evt.pickInfo.pickedPoint._z;
         console.log(x, y, z);
 
-        this.moveCam({x: x < 0 && x - 80 || x + 80, y: y < 0 && y - 80 || y + 80, z: z < 0 && z - 80 || z + 80});
+        this.moveCam({ x: x < 0 && x - 80 || x + 80, y: y < 0 && y - 80 || y + 80, z: z < 0 && z - 80 || z + 80 });
       }
     })
 
@@ -63,9 +70,36 @@ class Model {
   createScene() {
     this.camera.attachControl(this.canvas, true);
     this.scene.ambientColor = new BABYLON.Color3(1, 1, 1);
-    // this.light = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(1000, 1000, 1000), this.scene);
+    this.light = new BABYLON.HemisphericLight("Hemis", new BABYLON.Vector3(1000, 1000, 1000), this.scene);
+    console.log(this.light);
+    this.light.diffuse = new BABYLON.Color3(0.64, 0.64, 0.32);
+    this.light.intensity = 0.8;
 
-    BABYLON.SceneLoader.ImportMesh("", "./model1/model2/", "gift_giving.gltf", this.scene, (meshes, particleSystems, skeletons, animationGroups) => {
+    BABYLON.SceneLoader.ImportMesh("", "./model/model1/", "gift_giving.gltf", this.scene, (meshes, particleSystems, skeletons, animationGroups) => {
+
+      LOADING.classList.remove('active');
+
+      console.log(this.scene.getMeshByName('group3'))
+      let zoom1 = {
+        group1: this.scene.getMeshByName('group3'),
+      }
+      console.log(zoom1.group1)
+      meshes.forEach(item => {
+        if (item.name === 'ventole') {
+          item.rotationQuaternion = null;
+          this.rotateModelX(item, 4);
+        } else if (item.name === 'pale_ventola') {
+          item.rotationQuaternion = null;
+          this.rotateModelY(item, 4);
+        } else if (item.name === 'festone_dx' || item.name === 'festone_sx') {
+          item.rotationQuaternion = null;
+          this.rotateModelY(item, 0.1);
+        } else if (item.name === 'cono1.001') {
+          item.material.emissiveColor = new BABYLON.Color3(0.64, 0.64, 0.33);
+          item.rotationQuaternion = null;
+          this.rotateModelY(item, 0.05);
+        }
+      });
 
       // setTimeout(() => {
       //   this.moveCameraRight(toCel(this.camera.alpha), 0, 0, timeToFrame(1000));
@@ -75,12 +109,75 @@ class Model {
       // }, 5000);
       // setTimeout(() => {
       //   this.moveCameraRight(toCel(this.camera.alpha), 0, 0, timeToFrame(1000));
-      // }, 8000)
+      // }, 8000);\
+
+      x.addEventListener('change', (e) => {
+        this.cameraTarget.x = Number(e.target.value);
+        this.camera.target = new BABYLON.Vector3(this.cameraTarget.x, this.cameraTarget.y, this.cameraTarget.z);
+        console.log(this.cameraTarget)
+      });
+
+      y.addEventListener('change', (e) => {
+        this.cameraTarget.y = Number(e.target.value);
+        this.camera.target = new BABYLON.Vector3(this.cameraTarget.x, this.cameraTarget.y, this.cameraTarget.z);
+        console.log(this.cameraTarget)
+      });
+
+      z.addEventListener('change', (e) => {
+        this.cameraTarget.z= Number(e.target.value);
+        this.camera.target = new BABYLON.Vector3(this.cameraTarget.x, this.cameraTarget.y, this.cameraTarget.z);
+        console.log(this.cameraTarget)
+      });
+
+      // rightBtn.addEventListener('click', () => {
+      //   this.camera.target = new BABYLON.Vector3(this.cameraTarget.x, this.cameraTarget.y, this.cameraTarget.z);
+      //   console.log(this.cameraTarget)
+
+      //   // if (!isClicked) {
+      //   //   isClicked = true;
+      //   //   this.camera.target = new BABYLON.Vector3(0, 0, 0);
+      //   //   this.moveCameraRight(toCel(this.camera.alpha), 0, 0, timeToFrame(1000));
+      //   // }
+      // });
+      // leftBtn.addEventListener('click', () => {
+      //   this.cameraTarget.y -= 10;
+      //   this.camera.target = new BABYLON.Vector3(this.cameraTarget.x, this.cameraTarget.y, this.cameraTarget.z);
+      //   console.log(this.cameraTarget)
+
+      //   // if (!isClicked) {
+      //   //   isClicked = true;
+      //   //   this.camera.target = new BABYLON.Vector3(0, 0, 0);
+      //   //   this.moveCameraLeft(toCel(this.camera.alpha), 0, 0, timeToFrame(1000));
+      //   // }
+      // });
+
+      // midBtn.addEventListener('click', () => {
+      //   this.cameraTarget.z -= 10;
+      //   this.camera.target = new BABYLON.Vector3(this.cameraTarget.x, this.cameraTarget.y, this.cameraTarget.z);
+      //   console.log(this.cameraTarget)
+      //   // if (!isClicked) {
+      //   //   console.log(zoom1.group1)
+      //   //   this.camera.target = new BABYLON.Vector3(200, 200, 200);
+      //   // }
+      // });
     });
 
-    this.scene.createDefaultEnvironment();
-
+    // this.scene.createDefaultEnvironment();
+    console.log(this.scene)
+    this.scene.ambientColor = new BABYLON.Color3(1, 1, 1)
     return this.scene;
+  }
+
+  zoomOn(camPos) {
+    setTimeout(() => {
+      if (Math.abs(this.camera.position.x - camPos.x) > 5) {
+        console.log(camPos.x, this.camera.position.x)
+        this.camera.position = BABYLON.Vector3.Lerp(this.camera.position, new BABYLON.Vector3(camPos.x, camPos.y, camPos.z), 0.1);
+        this.moveCam(camPos);
+      } else {
+        console.log('stop!')
+      }
+    }, 15)
   }
 
   moveCameraLeft(s, v, t0, t) {
@@ -93,9 +190,23 @@ class Model {
       if (t0 <= t) {
         this.moveCameraLeft(s, v, t0, t);
       } else {
-        console.log(s);
+        isClicked = false;
       }
     }, FRAME)
+  }
+
+  rotateModelX(model, v) {
+    setTimeout(() => {
+      model.rotation.x += v;
+      this.rotateModelX(model, v)
+    }, 30)
+  }
+
+  rotateModelY(model, v) {
+    setTimeout(() => {
+      model.rotation.y += v;
+      this.rotateModelY(model, v)
+    }, 30)
   }
 
   moveCameraRight(s, v, t0, t) {
@@ -107,6 +218,8 @@ class Model {
       this.camera.alpha = BABYLON.Tools.ToRadians(s);
       if (t0 <= t) {
         this.moveCameraRight(s, v, t0, t);
+      } else {
+        isClicked = false;
       }
     }, FRAME)
   }
@@ -122,7 +235,7 @@ class Model {
       }
     }, 15)
   }
-  
+
   // rotateLeft()
 
 }
---
+++ script.js
@@ -0,0 +1,15 @@
+const LOADING = document.getElementById('loading-icon');
+
+var isClicked = false;
+
+const leftBtn = document.getElementById('rotate-left');
+const rightBtn = document.getElementById('rotate-right');
+const midBtn = document.getElementById('zoom-on');
+
+const x = document.getElementById('x');
+const y = document.getElementById('y');
+const z = document.getElementById('z');
+
+
+
+
